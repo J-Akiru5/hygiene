7975be254bf0494dc4d04e3cab042ef55289dbd4
J-Akiru5 <jeffmartinez@isufst.edu.ph>
2026-03-28 10:04:05 +0800

    Allow iframing and implement hover UX

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
